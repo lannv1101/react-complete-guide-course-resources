708c45d8f3b4ea30171d64b6b49764662160fbfe
lannv1101 <lanvunguyen1101@gmail.com>
2024-07-17 23:44:15 +0700

    Finished Course 13 React Quiz

---
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import QUESTIONS from "../questions";
+import { useCallback } from "react";
+import Question from "./Question";
+import Summary from "./Summary";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizComplete) {
+    return <Summary userAnswers={userAnswers} />;
+  }
+
+  return (
+    <div id="quiz">
+      <Question
+        key={activeQuestionIndex}
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+    </div>
+  );
+}
